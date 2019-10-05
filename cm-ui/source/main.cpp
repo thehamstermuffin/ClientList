e23a66fd6a3a6b48288dfe56e7386a976b9661d9
David <davidg0209@gmail.com>
2019-10-04 21:29:25 -0500

    cleaned ui main.cpp

--- cm-ui/source/main.cpp
+++ cm-ui/source/main.cpp
@@ -12,19 +12,10 @@ int main(int argc, char *argv[])
     qmlRegisterType<cm::controllers::MasterController>("CM", 1, 0, "MasterController");
 
     cm::controllers::MasterController masterController;
-//    QQmlApplicationEngine engine;
-//    engine.load(QUrl(QStringLiteral("qrc:/views/MasterView")));
-
 
     QQmlApplicationEngine engine;
     engine.rootContext()->setContextProperty("masterController",
     &masterController);
-    const QUrl url(QStringLiteral("qrc:/main.qml"));
-    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
-                     &app, [url](QObject *obj, const QUrl &objUrl) {
-        if (!obj && url == objUrl)
-            QCoreApplication::exit(-1);
-    }, Qt::QueuedConnection);
     engine.load(QUrl(QStringLiteral("qrc:views/MasterView.qml")));
 
     return app.exec();
