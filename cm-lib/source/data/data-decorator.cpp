bbd1572db2a0890a0b78376a5770acf18d56f267
David <davidg0209@gmail.com>
2020-09-18 22:48:27 -0500

    add dataDecorator

--- cm-lib/source/data/data-decorator.cpp
+++ cm-lib/source/data/data-decorator.cpp
@@ -1,6 +1,48 @@
+
 #include "data-decorator.h"
 
-DataDecorator::DataDecorator()
-{
+namespace cm {
+namespace data {
 
+class DataDecorator::Implementation
+{
+    public:
+    Implementation(Entity* _parent, const QString& _key, const QString& _label)
+        : parentEntity(_parent)
+        , key(_key)
+        , label(_label)
+    {
+    }
+
+    Entity* parentEntity{nullptr};
+    QString key;
+    QString label;
+
+};
+
+DataDecorator::DataDecorator(Entity* parent, const QString& key, const QString& label)
+    : QObject((QObject*)parent)
+{
+    implementation.reset(new Implementation(parent, key, label));
 }
+
+DataDecorator::~DataDecorator()
+{
+}
+
+const QString& DataDecorator::key() const
+{
+    return implementation->key;
+}
+
+const QString& DataDecorator::label() const
+{
+    return implementation->label;
+}
+
+Entity* DataDecorator::parentEntity()
+{
+    return implementation->parentEntity;
+}
+
+}}
