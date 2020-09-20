abe7de3d88ffb4419ac2820cfe3a434e837cf10b
David <davidg0209@gmail.com>
2020-09-20 11:38:58 -0500

    add address

--- cm-lib/source/models/address.h
+++ cm-lib/source/models/address.h
@@ -3,10 +3,34 @@
 
 #include <QObject>
 
-class Address
+#include <cm-lib_global.h>
+#include <data/string-decorator.h>
+#include <data/entity.h>
+
+namespace cm {
+namespace models {
+
+
+class CMLIBSHARED_EXPORT Address : public data::Entity
 {
+    QObject
+    QPROPERTY(cm::data::StringDecorator* ui_building MEMBER building CONSTANT)
+    QPROPERTY(cm::data::StringDecorator* ui_street MEMBER street CONSTANT)
+    QPROPERTY(cm::data::StringDecorator* ui_city MEMBER city CONSTANT)
+    QPROPERTY(cm::data::StringDecorator* ui_postcode MEMBER postcode CONSTANT)
+    QPROPERTY(cm::data::StringDecorator* ui_fullAddress MEMBER fullAddress CONSTANT)
 public:
-    Address();
+    explicit Address(QObject* parent = nullptr);
+
+    Address(QObject* parent, const QJsonObject& json);
+
+    data::StringDecorator* building{nullptr};
+    data::StringDecorator* street{nullptr};
+    data::StringDecorator* city{nullptr};
+    data::StringDecorator* postcode{nullptr};
+
+    QString fullAddress() const;
 };
 
+}}
 #endif // ADDRESS_H
