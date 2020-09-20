ef64cb5a9fb01fa17045004ce7f50f1692bc4085
David <davidg0209@gmail.com>
2020-09-20 16:45:59 -0500

    add new types to main ui

--- cm-ui/source/main.cpp
+++ cm-ui/source/main.cpp
@@ -5,6 +5,16 @@
 #include <framework/command.h>
 #include <controllers/command-controller.h>
 
+#include <models/address.h>
+#include <models/appointment.h>
+#include <models/client.h>
+#include <models/contact.h>
+
+#include <data/datetime-decorator.h>
+#include <data/enumerator-decorator.h>
+#include <data/int-decorator.h>
+#include <data/string-decorator.h>
+
 int main(int argc, char *argv[])
 {
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
@@ -15,6 +25,16 @@ int main(int argc, char *argv[])
     qmlRegisterType<cm::controllers::NavigationController>("CM", 1, 0, "NavigationController");
     qmlRegisterType<cm::controllers::CommandController>("CM", 1, 0, "CommandController");
     qmlRegisterType<cm::framework::Command>("CM", 1, 0, "Command");
+
+    qmlRegisterType<cm::data::DateTimeDecorator>("CM", 1, 0, "DateTimeDecorator");
+    qmlRegisterType<cm::data::EnumeratorDecorator>("CM", 1, 0, "EnumeratorDecorator");
+    qmlRegisterType<cm::data::IntDecorator>("CM", 1, 0, "IntDecorator");
+    qmlRegisterType<cm::data::StringDecorator>("CM", 1, 0, "StringDecorator");
+
+    qmlRegisterType<cm::models::Address>("CM", 1, 0, "Address");
+    qmlRegisterType<cm::models::Appointment>("CM", 1, 0, "Appointment");
+    qmlRegisterType<cm::models::Client>("CM", 1, 0, "Client");
+    qmlRegisterType<cm::models::Contact>("CM", 1, 0, "Contact");
 
     cm::controllers::MasterController masterController;
 
